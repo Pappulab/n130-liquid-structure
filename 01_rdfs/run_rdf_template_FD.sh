61848e1aeb32d17a4b2c4e9b32ba57dc90cffb8a
sam-wustl <77014779+sam-wustl@users.noreply.github.com>
2024-02-12 11:49:19 -0600

    Delete 01_rdfs/run_rdf_template_FD.sh

--- 01_rdfs/run_rdf_template_FD.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-initFile="##TOPODIR##/##SYSNAME##_init.xyz"
-trajFile="##RUNSDIR##/##SYSNAME##/Run_##REPSNUM##/##SYSNAME##_##REPSNUM##_nvt.dcd"
-
-vmd "${initFile}" "${trajFile}" -dispdev text -e rdf_FD.tk
