d9027e158c33fe4c81b263ff5c8c46da8aeed969
sam-wustl <77014779+sam-wustl@users.noreply.github.com>
2024-02-12 11:48:06 -0600

    Add files via upload

---
+++ 02_degrees/01_generate_dirs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+thisDir=$(pwd)
+runsDir="XX_XX"
+topoDir="XX_XX"
+
+sysNames=(
+"rpL5"
+"no_A0"
+"no_A1"
+"no_A2"
+)
+
+for sysName in "${sysNames[@]}"
+do
+    for j in 1 2 3 4 5
+    do
+        mkdir -p "${thisDir}/${sysName}/Run_${j}/"
+    done
+done
+    
---
+++ 02_degrees/02_generate_charged_analyses.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+thisDir=$(pwd)
+runsDir="XX_XX"
+topoDir="XX_XX"
+
+trajDir="${runsDir}"
+
+sysNames=("rpL5" "no_A0" "no_A1" "no_A2")
+
+for sysName in "${sysNames[@]}"
+do
+    for repNum in 1 2 3 4 5
+    do
+        sed -e "s@##SYSNAME##@${sysName}@g" \
+            -e "s@##REPSNUM##@${repNum}@g"  \
+            -e "s@##RUNSDIR##@${trajDir}@g" \
+            -e "s@##TOPODIR##@${topoDir}@g" \
+            network_template_charge.py > \
+             "${sysName}/Run_${repNum}/network_charge.py"
+             done
+done
+     
